cfc3052173cfc2a38ac299ad035c75b7a765b8be
Maksim Zinchenko <zinchenkomax@ukr.net>
2016-01-24 18:56:01 +0200

    Frontend - to  currency

--- yii-application/frontend/views/rate/index.php
+++ yii-application/frontend/views/rate/index.php
@@ -14,19 +14,43 @@ $form = ActiveForm::begin([
     'id' => 'login-form',
     'options' => ['class' => 'form-horizontal'],
 ]) ?>
-    <?= $form->field( $currency, 'billing_id' )->dropDownList(
-        $billing,
-        [
+
+<!-- from_billing --!>
+    <?= $form->field( $currency, 'billing_id', [
+            'inputOptions' => ['class' => 'form-control','id' => 'from_billing']
+        ]
+    )->dropDownList( $billing, [
             ['id' => 'base-billing'],
-            'prompt'=>'Выберите платежную систему',
+            'prompt'=>'Выберите базовую платежную систему',
         ]
     )->label('Название базовой платежной системы'); ?>
 
-    <?= $form->field( $rate, 'from_currency_id' )->dropDownList(
-        [],
-        [
-//            ['id' => 'base_currency'],
+<!-- from_currency --!>
+    <?= $form->field( $rate, 'from_currency_id' , [
+            'inputOptions' => [ 'class' => 'form-control','id' => 'from_currency' ]
+        ]
+    )->dropDownList( [], [
             'prompt'=>'Выберите базовую валюту',
+            'size' => 10,
+        ]
+    ) ?>
+
+<!-- to_billing --!>
+    <?= $form->field( $currency, 'billing_id', [
+            'inputOptions' => ['class' => 'form-control','id' => 'to_billing']
+    ]
+    )->dropDownList( $billing, [
+            ['id' => 'base-billing'],
+            'prompt'=>'Выберите платежную систему конвертации',
+        ]
+    )->label('Название платежной системы конвертации'); ?>
+
+<!-- to_currency --!>
+    <?= $form->field( $rate, 'to_currency_id', [
+            'inputOptions' => [ 'class' => 'form-control','id' => 'to_currency' ]
+        ]
+    )->dropDownList( [], [
+            'prompt'=>'Выберите валюту конвертации',
             'size' => 10,
         ]
     ) ?>
--- yii-application/frontend/web/js/rate.js
+++ yii-application/frontend/web/js/rate.js
@@ -3,20 +3,36 @@
  */
 'use strict';
 jQuery(function($){
-    $("#currency-billing_id").on("change", function(){
-        var drop_down_base = $("#currency-billing_id");
-        var billing_id = drop_down_base.val();
-        var billing_name = drop_down_base.find( "option:selected").text();
-        var data1 = '';
-        var select_base = $('#rate-from_currency_id');
-        $.post( "rate/get-currencies", { "billing_id": billing_id }, function( data ){
+    var drop_down_from = $("#from_billing");
+    var drop_down_to = $("#to_billing");
+    var select_from = $('#from_currency');
+    var select_to = $('#to_currency');
 
-            select_base.empty();
-            var options = select_base.prop('options');
+    drop_down_from.on("change", function(){
+        var billing_id = drop_down_from.val();
+        var billing_name = drop_down_from.find( "option:selected").text();
+
+        $.post( "rate/get-currencies", { "billing_id": billing_id }, function( data ){
+            select_from.empty();
+            var options = select_from.prop('options');
+
             $.each(data, function(val, text) {
                 options[options.length] = new Option( billing_name + ' - ' + text, val );
             });
         } );
+    });
 
+    drop_down_to.on("change", function(){
+        var billing_id = drop_down_to.val();
+        var billing_name = drop_down_to.find( "option:selected").text();
+
+        $.post( "rate/get-currencies", { "billing_id": billing_id }, function( data ){
+            select_to.empty();
+            var options = select_to.prop('options');
+
+            $.each(data, function(val, text) {
+                options[options.length] = new Option( billing_name + ' - ' + text, val );
+            });
+        } );
     });
 });
